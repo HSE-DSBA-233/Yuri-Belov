359ba5d4dc1518f33883d957a8afa7c1ddee2392
KekaSick <mlggg@bk.ru>
2024-03-09 13:48:59 +0300

    Added math books

--- src/handlers/math.py
+++ src/handlers/math.py
@@ -4,6 +4,8 @@ from aiogram.enums import ParseMode
 from utils.logging import handler
 from keyboards.inline.math import InlineKeyboards
 from filters.chat_type import ChatTypeFilter
+from aiogram.types import FSInputFile
+
 
 math_router = Router(name='math')
 math_router.message.filter(ChatTypeFilter(chat_type=["private"]))
@@ -30,7 +32,116 @@ async def programming_theory_handler(callback: types.CallbackQuery):
     await callback.message.edit_text(text="Теория", reply_markup=InlineKeyboards().math_theory())
 
 
-# callback on theory button from, math menu
+""""""
+#Fixtengolz
+@math_router.callback_query(F.data == "math_book_analysis")
+async def send_pdf(callback: types.CallbackQuery):
+    handler(__name__, type=callback)
+    await callback.message.answer("Отправка книги, пожалуйста подождите...")
+    await callback.message.reply_document(FSInputFile(path="src/assets/mathBooks/1968Fixtengolz1-2_compressed.pdf"))
+
+
+@math_router.callback_query(F.data == "math_book_algebra")
+async def send_pdf(callback: types.CallbackQuery):
+    handler(__name__, type=callback)
+    await callback.message.answer("Отправка книги, пожалуйста подождите...")
+    await callback.message.reply_document(FSInputFile(path="src/assets/mathBooks/Kostrikin_A_I_-_Vvedenie_v_algebru_Chast_I_Osnovy_algebry_2000_FIZMATLIT_compressed.pdf"))
+
+
+@math_router.callback_query(F.data == "math_book_geometry")
+async def send_pdf(callback: types.CallbackQuery):
+    handler(__name__, type=callback)
+    await callback.message.answer("Отправка книги, пожалуйста подождите...")
+    await callback.message.reply_document(FSInputFile(path="src/assets/mathBooks/TopologyAndGeometry.pdf"))
+
+
+@math_router.callback_query(F.data == "math_book_differential")
+async def send_pdf(callback: types.CallbackQuery):
+    handler(__name__, type=callback)
+    await callback.message.answer("Отправка книги, пожалуйста подождите...")
+    await callback.message.reply_document(FSInputFile(path="src/assets/mathBooks/differentialPt1.pdf"))
+    await callback.message.reply_document(FSInputFile(path="src/assets/mathBooks/differentialPt2.pdf"))
+
+
+@math_router.callback_query(F.data == "math_functions")
+async def send_pdf(callback: types.CallbackQuery):
+    handler(__name__, type=callback)
+    await callback.message.answer("Отправка книги, пожалуйста подождите...")
+    await callback.message.reply_document(FSInputFile(path="src/assets/mathBooks/Volkovyskiy_L_I__Lunts_G_L__Aramanovich_I_G_-_Sbornik_zadach_po_teorii_funktsiy_komplexnogo_peremennogo_-_2004_compressed.pdf"))
+
+
+@math_router.callback_query(F.data == "math_book_functionsAnalysis")
+async def send_pdf(callback: types.CallbackQuery):
+    handler(__name__, type=callback)
+    await callback.message.answer("Отправка книги, пожалуйста подождите...")
+    await callback.message.reply_document(FSInputFile(path="src/assets/mathBooks/FunctionalAnalysis.pdf"))
+
+
+@math_router.callback_query(F.data == "math_book_probability")
+async def send_pdf(callback: types.CallbackQuery):
+    handler(__name__, type=callback)
+    await callback.message.answer("Отправка книги, пожалуйста подождите...")
+    await callback.message.reply_document(FSInputFile(path="src/assets/mathBooks/ProbTheory_compressed.pdf"))
+
+
+@math_router.callback_query(F.data == "math_book_numberMethods")
+async def send_pdf(callback: types.CallbackQuery):
+    handler(__name__, type=callback)
+    await callback.message.answer("Отправка книги, пожалуйста подождите...")
+    await callback.message.reply_document(FSInputFile(path="src/assets/mathBooks/Samarskiy_1989_432_compressed.pdf"))
+
+
+@math_router.callback_query(F.data == "math_book_discrete")
+async def send_pdf(callback: types.CallbackQuery):
+    handler(__name__, type=callback)
+    await callback.message.answer("Отправка книги, пожалуйста подождите...")
+    await callback.message.reply_document(FSInputFile(path="src/assets/mathBooks/discra_compressed.pdf"))
+
+
+@math_router.callback_query(F.data == "math_book_logic")
+async def send_pdf(callback: types.CallbackQuery):
+    handler(__name__, type=callback)
+    await callback.message.answer("Отправка книги, пожалуйста подождите...")
+    await callback.message.reply_document(FSInputFile(path="src/assets/mathBooks/MATLOGIC.pdf"))
+
+
+@math_router.callback_query(F.data == "math_book_diffGeometry")
+async def send_pdf(callback: types.CallbackQuery):
+    handler(__name__, type=callback)
+    await callback.message.answer("Отправка книги, пожалуйста подождите...")
+    await callback.message.reply_document(FSInputFile(path=""))
+
+
+@math_router.callback_query(F.data == "math_numberTheory")
+async def send_pdf(callback: types.CallbackQuery):
+    handler(__name__, type=callback)
+    await callback.message.answer("Отправка книги, пожалуйста подождите...")
+    await callback.message.reply_document(FSInputFile(path="src/assets/mathBooks/osnovy_teorii_chisel.pdf"))
+
+
+@math_router.callback_query(F.data == "math_book_optimization")
+async def send_pdf(callback: types.CallbackQuery):
+    handler(__name__, type=callback)
+    await callback.message.answer("Отправка книги, пожалуйста подождите...")
+    await callback.message.reply_document(FSInputFile(path="src/assets/mathBooks/Elsgolc1969ru_compressed.pdf"))
+
+
+@math_router.callback_query(F.data == "math_book_managment")
+async def send_pdf(callback: types.CallbackQuery):
+    handler(__name__, type=callback)
+    await callback.message.answer("Отправка книги, пожалуйста подождите...")
+    await callback.message.reply_document(FSInputFile(path="src/assets/mathBooks/Managment.pdf"))
+
+
+@math_router.callback_query(F.data == "math_book_physics")
+async def send_pdf(callback: types.CallbackQuery):
+    handler(__name__, type=callback)
+    await callback.message.answer("Отправка книги, пожалуйста подождите...")
+    await callback.message.reply_document(FSInputFile(path="src/assets/mathBooks/vladimirov-lectures-1981_compressed.pdf"))
+""""""
+
+
+# callback on tasks button from, math menu
 @math_router.callback_query(F.data == "math_tasks")
 async def programming_tasks_handler(callback: types.CallbackQuery):
     handler(__name__, type=callback)
