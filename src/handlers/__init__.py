b537b0305405df41ec88645bfd8306e25aaa56b8
KekaSick <mlggg@bk.ru>
2024-03-11 23:35:16 +0300

    Made some structure

--- src/handlers/__init__.py
+++ src/handlers/__init__.py
@@ -1,11 +1,15 @@
-from .start import start_router
-from .math import math_router
-from .programming import programming_router
-from .history import history_router
-from .theory import theory_router
+from .start.start import start_router
+from .math.math import math_router
+from .programming.programming import programming_router
+from .history.history import history_router
+from .programming.theory import theory_router
+from .math.math_books import math_books_router
+from .math.math_tasks import math_tasks_router
 routers = (
     start_router,
     math_router,
+    math_books_router,
+    math_tasks_router,
     programming_router,
     history_router,
     theory_router
---
+++ src/handlers/math/math.py
@@ -0,0 +1,33 @@
+from aiogram import Router, types, F
+from aiogram.filters import Command
+from aiogram.enums import ParseMode
+from utils.logging import handler
+from aiogram.filters import StateFilter
+from keyboards.inline.math import InlineKeyboards
+from states.math_state import MathState
+from filters.chat_type import ChatTypeFilter
+from aiogram.types import FSInputFile
+from aiogram.fsm.context import FSMContext
+import json
+import random
+from data.config import conf
+from utils.db_math import db
+
+
+math_router = Router(name='math')
+math_router.message.filter(ChatTypeFilter(chat_type=["private"]))
+
+
+# default keyboard
+@math_router.message(F.text == "Математика")
+async def math_handler_menu(message: types.Message):
+    handler(__name__, type=message)
+    await message.answer("Выберите раздел:", reply_markup=InlineKeyboards().math_menu())
+
+
+# callback on math
+@math_router.callback_query(F.data == "math")
+async def math_handler_menu(callback: types.CallbackQuery):
+    handler(__name__, type=callback)
+    await callback.message.edit_text("Выберите раздел:", reply_markup=InlineKeyboards().math_menu())
+
